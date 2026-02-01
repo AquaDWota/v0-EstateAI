abca487c0775f6ae731d5968a4454628d4d80a1c
LowkiMischeif <tyler.ferreira@gmail.com>
2026-01-31 22:43:46 -0500

    can now search for zipcodes properly and connected to mongoDB

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,6 @@ from .models import (
 app = FastAPI(title="New England Deal Underwriter API")
 mongo = get_properties_collection()
 
-
 @app.exception_handler(HTTPException)
 def _http_exception_handler(_: Request, exc: HTTPException) -> JSONResponse:
     return JSONResponse(status_code=exc.status_code, content={"error": exc.detail})
@@ -51,57 +50,9 @@ def analyze_properties_route(payload: AnalyzePropertiesRequest):
 
 @app.get("/api/properties/{zip_code}")
 def get_properties(zip_code: str):
-    print(zip_code)
-    return [
-      {
-        "id": "prop-0001",
-        "address": "121 Summer Street",
-        "zipCode": "06114",
-        "lat": 41.721884,
-        "lng": -72.694396,
-        "listPrice": 915888,
-        "bedrooms": 2,
-        "bathrooms": 2,
-        "sqft": 3188,
-        "propertyType": "condo",
-        "yearBuilt": 2006,
-        "estimatedRent": 6577,
-        "propertyTaxPerYear": 10285,
-        "insurancePerYear": 4107,
-        "hoaPerYear": 6726
-      },
-      {
-        "id": "prop-0002",
-        "address": "331 Hill Road",
-        "zipCode": "02125",
-        "lat": 42.315568,
-        "lng": -71.06041,
-        "listPrice": 1233536,
-        "bedrooms": 5,
-        "bathrooms": 3,
-        "sqft": 2492,
-        "propertyType": "townhouse",
-        "yearBuilt": 1983,
-        "estimatedRent": 7681,
-        "propertyTaxPerYear": 15747,
-        "insurancePerYear": 6089,
-        "hoaPerYear": 6097
-      },
-      {
-        "id": "prop-0003",
-        "address": "117 School Street",
-        "zipCode": "02136",
-        "lat": 41.783125,
-        "lng": -71.425447,
-        "listPrice": 1621125,
-        "bedrooms": 2,
-        "bathrooms": 2,
-        "sqft": 3298,
-        "propertyType": "single-family",
-        "yearBuilt": 1950,
-        "estimatedRent": 7991,
-        "propertyTaxPerYear": 16378,
-        "insurancePerYear": 6153,
-        "hoaPerYear": 0
-      },
-    ]
+    properties = list(mongo.find({"zipCode": zip_code}))
+    if not properties:
+        return {}
+    for prop in properties:
+        prop["_id"] = str(prop["_id"])  # Convert ObjectId to string for JSON serialization
+    return properties
